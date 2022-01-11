b4ddbbce8ab3de06c389d8ca828e02b823d4ebaa
didierganthier <didierganthierperan@gmail.com>
2022-01-11 12:54:55 -0500

    adding movements methods

--- lib/main.dart
+++ lib/main.dart
@@ -65,6 +65,67 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void updatedDirection() {
+    setState(() {
+      //update vertical dirction
+      if (bally >= 0.9 && playerX + brickWidth >= ballx && playerX <= ballx) {
+        ballYDirection = direction.UP;
+      } else if (bally <= -0.9) {
+        ballYDirection = direction.DOWN;
+      }
+      // update horizontal directions
+      if (ballx >= 1) {
+        ballXDirection = direction.LEFT;
+      } else if (ballx <= -1) {
+        ballXDirection = direction.RIGHT;
+      }
+    });
+  }
+
+  void moveBall() {
+    //vertical movement
+    setState(() {
+      if (ballYDirection == direction.DOWN) {
+        bally += 0.01;
+      } else if (ballYDirection == direction.UP) {
+        bally -= 0.01;
+      }
+    });
+    //horizontal movement
+    setState(() {
+      if (ballXDirection == direction.LEFT) {
+        ballx -= 0.01;
+      } else if (ballXDirection == direction.RIGHT) {
+        ballx += 0.01;
+      }
+    });
+  }
+
+  void moveLeft() {
+    setState(() {
+      if (!(playerX - 0.1 <= -1)) {
+        playerX -= 0.1;
+      }
+    });
+  }
+
+  void moveRight() {
+    if (!(playerX + brickWidth >= 1)) {
+      playerX += 0.1;
+    }
+  }
+
+  void resetGame() {
+    Navigator.pop(context);
+    setState(() {
+      gameStarted = false;
+      ballx = 0;
+      bally = 0;
+      playerX = -0.2;
+      enemyX = -0.2;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
